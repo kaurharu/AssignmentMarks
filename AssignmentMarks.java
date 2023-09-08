d5a32bb1b768c8a96fdff294fc8298d3bcc351b9
Harinder Kaur <harinder031998kaur@gmail.com>
2023-09-08 13:59:40 +1000

    Create AssignmentMarks.java

---
+++ AssignmentMarks.java
@@ -0,0 +1,13 @@
+//import needed libraries
+
+//Create a class for Assignment Marks Program
+public class AssignmentMarks
+{
+    public void EnterAssignmentName()
+    {
+        System.out.println("Welcome");          //Welcome statemnet for program
+    }
+    public static void main(String[] args) {
+        
+    }
+}
