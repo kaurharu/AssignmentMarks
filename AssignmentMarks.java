4acf51ad3cc0365206f982b28958b1b121b7db65
Harinder Kaur <harinder031998kaur@gmail.com>
2023-09-08 17:03:43 +1000

    Update AssignmentMarks.java

--- AssignmentMarks.java
+++ AssignmentMarks.java
@@ -6,17 +6,32 @@ import java.util.Scanner;
 
 public class AssignmentMarks
 {
-    public void EnterAssignmentName()
+    //declare variable in class to use variable globally
+    String assignmentName;                          //declare variable to store Assignmnet name
+    double [] marks = new double[30];               //declare array to store marks
+
+    public void EnterAssignmentName()              //Method for receiving assignmnet name (f1)
     {
-        System.out.println("Welcome");          //Welcome statemnet for program
-        String assignmentName;
+        System.out.println("Welcome");          //Welcome statemnet for program                  
         System.out.println("Please Enter the Name of the Assignment");
         Scanner input = new Scanner(System.in);
-        assignmentName = input.nextLine();
-        System.out.println("assignment name : " + assignmentName);
+        assignmentName = input.nextLine();        //Received assignment name
+    }
+
+    public void EnterStudentMarks()
+    {
+        System.out.println("Please enter the marks for " + assignmentName + " 30 students");
+        int i;                                  //declare variable for iteration
+        Scanner input = new Scanner(System.in);
+        for(i=0;i<30;i++)
+        {
+            System.out.println("Enter Assignmnet marks for student " + i+1);
+            marks[i]=input.nextDouble();
+        }
     }
     public static void main(String[] args) {
         AssignmentMarks assignmentObject = new AssignmentMarks();
         assignmentObject.EnterAssignmentName();
+        assignmentObject.EnterStudentMarks();
     }
 }
