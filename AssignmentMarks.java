41b6bdf9ace5c7b38ab866a89378cb63b23bc42d
Harinder Kaur <harinder031998kaur@gmail.com>
2023-09-08 14:06:31 +1000

    Update AssignmentMarks.java

--- AssignmentMarks.java
+++ AssignmentMarks.java
@@ -1,13 +1,22 @@
 //import needed libraries
 
 //Create a class for Assignment Marks Program
+
+import java.util.Scanner;
+
 public class AssignmentMarks
 {
     public void EnterAssignmentName()
     {
         System.out.println("Welcome");          //Welcome statemnet for program
+        String assignmentName;
+        System.out.println("Please Enter the Name of the Assignment");
+        Scanner input = new Scanner(System.in);
+        assignmentName = input.nextLine();
+        System.out.println("assignment name : " + assignmentName);
     }
     public static void main(String[] args) {
-        
+        AssignmentMarks assignmentObject = new AssignmentMarks();
+        assignmentObject.EnterAssignmentName();
     }
 }
