a6e1ef83603d752b0e5143a75b4f5ae05ea9d9e9
Harinder Kaur <harinder031998kaur@gmail.com>
2023-09-11 14:39:21 +1000

    Update AssignmentMarks.java

--- AssignmentMarks.java
+++ AssignmentMarks.java
@@ -10,7 +10,7 @@ public class AssignmentMarks
     String assignmentName;                          //declare variable to store Assignmnet name
     double [] marks = new double[30];               //declare array to store marks
     double min, max, mean, standardDeviation, variance;     //declare variable to perform given functions
-    int i, studentHighest = 0, studentLowest = 0;                                  //declare variable for iteration
+    int i, studentHighest = 1, studentLowest = 1;                                  //declare variable for iteration
         
     public void EnterAssignmentName()              //F1: Method for receiving assignmnet name (f1)
     {
@@ -24,7 +24,7 @@ public class AssignmentMarks
     {
         System.out.println("Please enter the marks of " + assignmentName + " for 30 students");
         Scanner input = new Scanner(System.in);
-        for(i=0;i<30;i++)
+        for(i=0;i<10;i++)
         {
             System.out.println("Enter " + assignmentName +" marks for student " + (i+1));
             double temp = input.nextDouble();
@@ -42,7 +42,7 @@ public class AssignmentMarks
 
     public void DisplayMarks()                          //F4: displaying marks
     {
-        for(i=0;i<30;i++)
+        for(i=0;i<10;i++)
         {
             System.out.println("Marks of student " + (i+1) + " in " + this.assignmentName + ": " + this.marks[i]);
         }
@@ -52,46 +52,62 @@ public class AssignmentMarks
     {
         min = marks[0];
         max = marks[0];
-        for(i=0;i<30;i++)
+        for(i=0;i<10;i++)
         {
-            if(marks[i] >= max)
-            {
-                if(max > marks[i])
-                max = marks[i];
-                else if(max == marks[i])
-                studentHighest = studentHighest + 1;
+                if(max <= marks[i])
+                {
+                    if (max <= marks [i])
+                    {
+                        max = marks[i];
+                        studentHighest = 1;
+                    }
+                    else if(max == marks[i])
+                    {
+                    studentHighest = studentHighest + 1;
+                    max = marks[i];
+                    }
+                }
+                else if(min >= marks[i])
+                {
+                   if(min > marks[i])
+                   {
+                    min = marks[i];
+                    studentLowest = 1;
+                   }
+                    else if(min == marks[i])
+                    {
+                    studentLowest = studentLowest + 1;
+                    min = marks[i];
+                    }
+                }
             }
-            else if(marks[i] <= min)
-            {
-                if(min < marks[i])
-                min = marks[i];
-                else if(min == marks[i])
-                studentLowest = studentLowest + 1;
-            }
-        }
+            System.out.println(min);
+            System.out.println(max);
+            System.out.println(studentHighest);
+            System.out.println(studentLowest);
         System.out.println("The hightest marks obtained by " + this.studentHighest + " students in " + this.assignmentName + ": " + this.max);
-        System.out.println("The lowest marks obtained by " + this.studentLowest + " studnets in " + this.assignmentName + ": " + this.min);
+        System.out.println("The lowest marks obtained by " + this.studentLowest + " students in " + this.assignmentName + ": " + this.min);
     }
 
     public void MeanCalculate()         //F6: finding mean of all marks obtained
     {
         double sum = 0;
-        for(i=0;i<30;i++)
+        for(i=0;i<10;i++)
         {
             sum = sum + marks[i];
         }
-        mean = sum/30;
+        mean = sum/10;
         System.out.println("The mean of all marks obtained " + this.mean);
     }
 
     public void StandardDeviationCalculation()              //F6: finding Standarad deviation
     {
         double deviation = 0;
-        for(i=0;i<30;i++)
+        for(i=0;i<10;i++)
         {
             deviation = deviation + ((marks[i] - mean) * (marks[i] - mean));
         }
-        variance = deviation / 30;
+        variance = deviation / 10;
         standardDeviation = Math.sqrt(variance);
         System.out.println("The standard deviation of all marks obtained " + this.standardDeviation);
     }
